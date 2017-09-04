cd4e008edd2f36add3de6f0eeb7dbf035346edbb
Chris Yap <chris.yap@medicaldirector.com>
2017-09-04 18:19:22 +1000

    update badges layout

--- src/components/pages/Badges.vue
+++ src/components/pages/Badges.vue
@@ -2,144 +2,100 @@
   <div id="badges">
     <h1 class="hx-title">Badges</h1>
     <h2 class="hx-subtitle">Small and adaptive tag for adding context to just about any content.</h2>
+
     <hr>
-    <h1 class="h3">Badge examples</h1>
-    <div class="hx-columns">
-      <div class="hx-column is-6">
-        <p>Badges for text</p>
-        <div class="doc-example has-text-centered">
-          <div class="doc-tooltip-demo">
-            <span class="hx-badge">Default</span>
-            <span class="hx-badge is-white">White</span>
-            <span class="hx-badge is-outlined">Outlined</span>
-            <span class="hx-badge is-dark">Dark</span>
-            <span class="hx-badge is-black">Black</span>
-            <span class="hx-badge is-primary">Primary</span>
-            <span class="hx-badge is-info">Info</span>
-            <span class="hx-badge is-warning">Warning</span>
-            <span class="hx-badge is-danger">Danger</span>
-          </div>
-        </div>
-      </div>
-      <div class="hx-column is-6">
-        <p>Code</p>
-        <pre><code class="language-markup"><xmp>
-        <span class="hx-badge">Default</span>
-        <span class="hx-badge is-white">White</span>
-        <span class="hx-badge is-outlined">Outlined</span>
-        <span class="hx-badge is-dark">Dark</span>
-        <span class="hx-badge is-black">Black</span>
-        <span class="hx-badge is-primary">Primary</span>
-        <span class="hx-badge is-info">Info</span>
-        <span class="hx-badge is-warning">Warning</span>
-        <span class="hx-badge is-danger">Danger</span>
-        </xmp></code></pre>
-      </div>
-    </div>
-    <br>
 
-    <div class="hx-columns">
-      <div class="hx-column is-6">
-        <p>Badges for numerical values</p>
-        <div class="doc-example has-text-centered">
-          <div class="doc-tooltip-demo">
-            <span class="hx-badge">0</span>
-            <span class="hx-badge is-white">34</span>
-            <span class="hx-badge is-outlined">83</span>
-            <span class="hx-badge is-dark">87</span>
-            <span class="hx-badge is-black">59</span>
-            <span class="hx-badge is-primary">99+</span>
-            <span class="hx-badge is-info">3</span>
-            <span class="hx-badge is-warning">6</span>
-            <span class="hx-badge is-danger">2</span>
-          </div>
-        </div>
-      </div>
-      <div class="hx-column is-6">
-        <p>Code</p>
-        <pre><code class="language-markup"><xmp>
-        <span class="hx-badge">0</span>
-        <span class="hx-badge is-white">34</span>
-        <span class="hx-badge is-outlined">83</span>
-        <span class="hx-badge is-dark">87</span>
-        <span class="hx-badge is-black">59</span>
-        <span class="hx-badge is-primary">99+</span>
-        <span class="hx-badge is-info">3</span>
-        <span class="hx-badge is-warning">6</span>
-        <span class="hx-badge is-danger">2</span>
-        </xmp></code></pre>
-      </div>
-    </div>
+    <expanding-code-example v-for="(example, i) in examples" key="i"
+      :title="example.title"
+      :code="example.code"
+      :example="example.code"
+      :visible="false"
+    ></expanding-code-example>
 
-    <div class="hx-columns">
-      <div class="hx-column is-6">
-        <h4>Sizes</h4>
-        <p>There are 2 additional sizes</p>
-        <div class="doc-example has-text-centered">
-          <div class="doc-tooltip-demo">
-            <p>
-              <span class="hx-badge is-outlined is-medium">Medium</span>
-            </p>
-            <p>
-              <span class="hx-badge is-danger is-large">Large</span>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="hx-column is-6">
-        <h4>&nbsp;</h4>
-        <p>Code</p>
-        <pre><code class="language-markup"><xmp>
-        <span class="hx-badge is-outlined is-medium">Medium</span>
-        <span class="hx-badge is-danger is-large">Large</span>
-        </xmp></code></pre>
-      </div>
-    </div>
-
-    <div class="hx-columns">
-      <div class="hx-column is-6">
-        <p>Add a delete button</p>
-        <div class="doc-example has-text-centered">
-          <div class="doc-tooltip-demo">
-            <p>
-              <span class="hx-badge is-outlined">Default
-                <button class="hx-delete is-small"></button>
-              </span>
-            </p>
-            <p>
-              <span class="hx-badge is-warning is-medium">Medium
-                <button class="hx-delete is-small"></button>
-              </span>
-            </p>
-            <p>
-              <span class="hx-badge is-danger is-large">Large
-                <button class="hx-delete"></button>
-              </span>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="hx-column is-6">
-        <h4>&nbsp;</h4>
-        <p>Code</p>
-        <pre><code class="language-markup"><xmp>
-        <span class="hx-badge is-outlined">Default
-          <button class="hx-delete is-small"></button>
-        </span>
-        <span class="hx-badge is-warning is-medium">Medium
-          <button class="hx-delete is-small"></button>
-        </span>
-        <span class="hx-badge is-danger is-large">Large
-          <button class="hx-delete"></button>
-        </span>
-        </xmp></code></pre>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
+import ExpandingCodeExample from '@/components/ExpandingCodeExample'
 export default {
+  data () {
+    return {
+      examples: [
+        {
+          title: 'Badges for text',
+          code: `
+<div class="has-text-centered">
+  <span class="hx-badge">Default</span>
+  <span class="hx-badge is-white">White</span>
+  <span class="hx-badge is-outlined">Outlined</span>
+  <span class="hx-badge is-dark">Dark</span>
+  <span class="hx-badge is-black">Black</span>
+  <span class="hx-badge is-primary">Primary</span>
+  <span class="hx-badge is-info">Info</span>
+  <span class="hx-badge is-warning">Warning</span>
+  <span class="hx-badge is-danger">Danger</span>
+</div>
+          `
+        },
+        {
+          title: 'Badges for numerical values',
+          code: `
+<div class="has-text-centered">
+  <span class="hx-badge">0</span>
+  <span class="hx-badge is-white">34</span>
+  <span class="hx-badge is-outlined">83</span>
+  <span class="hx-badge is-dark">87</span>
+  <span class="hx-badge is-black">59</span>
+  <span class="hx-badge is-primary">99+</span>
+  <span class="hx-badge is-info">3</span>
+  <span class="hx-badge is-warning">6</span>
+  <span class="hx-badge is-danger">2</span>
+</div>
+          `
+        },
+        {
+          title: 'Sizes',
+          code: `
+<div class="has-text-centered">
+  <p>There are 2 additional sizes</p>
+  <p>
+    <span class="hx-badge is-outlined is-medium">Medium</span>
+  </p>
+  <p>
+    <span class="hx-badge is-danger is-large">Large</span>
+  </p>
+</div>
+          `
+        },
+        {
+          title: 'Add a delete button',
+          code: `
+<div class="has-text-centered">
+  <p>Add a delete button</p>
+  <p>
+    <span class="hx-badge is-outlined">Default
+      <button class="hx-delete is-small"></button>
+    </span>
+  </p>
+  <p>
+    <span class="hx-badge is-warning is-medium">Medium
+      <button class="hx-delete is-small"></button>
+    </span>
+  </p>
+  <p>
+    <span class="hx-badge is-danger is-large">Large
+      <button class="hx-delete"></button>
+    </span>
+  </p>
+</div>
+          `
+        }
+      ]
+    }
+  },
+  components: {
+    ExpandingCodeExample
+  }
 }
 </script>
 
