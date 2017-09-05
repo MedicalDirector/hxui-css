60eded61e6def50cbdd6ae0b48cf40fef9ce19d7
Chris Yap <chris.yap@medicaldirector.com>
2017-09-05 18:24:07 +1000

    Update Nav page layout

--- src/components/pages/Nav.vue
+++ src/components/pages/Nav.vue
@@ -2,142 +2,102 @@
   <div id="nav">
     <h1 class="hx-title">Nav</h1>
     <h2 class="hx-subtitle">A responsive horizontal nav bar that can contain links, tabs, buttons, icons, and a logo</h2>
-    <hr>
-
-    <div class="hx-columns">
-      <div class="hx-column">
-
-        <h3 class="">Basic nav</h3>
-        <p>Note the nav requires the <code>.hx-nav</code> base class.</p>
-        <br>
-
-        <p>Example</p>
-        <ul class="hx-nav">
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link">News & data</a>
-          </li>
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link is-active">Status board</a>
-          </li>
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link">Custom</a>
-          </li>
-        </ul>
-        <br>
-
-        <p>Code</p>
-        <pre><code class="language-markup"><xmp>
-    <ul class="hx-nav">
-      <li class="hx-nav-item">
-        <a href="#" class="hx-nav-link">News & data</a>
-      </li>
-      <li class="hx-nav-item">
-        <a href="#" class="hx-nav-link is-active">Status board</a>
-      </li>
-      <li class="hx-nav-item">
-        <a href="#" class="hx-nav-link">Custom</a>
-      </li>
-    </ul>
-        </xmp></code></pre>
-
-        <br>
-        <H4>Modifiers</H4>
-        <h5>Center aligned</h5>
-        <p>Centering the nav can be easily done by adding the modifier <code>.is-centered</code> to the <code>.hx-nav</code> base class.</p>
-
-        <br>
-        <p>Example</p>
-        <ul class="hx-nav is-centered">
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link">News & data</a>
-          </li>
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link is-active">Status board</a>
-          </li>
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link">Custom</a>
-          </li>
-        </ul>
-        <br>
-
-        <p>Code</p>
-        <pre><code class="language-markup"><xmp>
-      <ul class="hx-nav is-centered">
-        ...
-      </ul>
-        </xmp></code></pre>
-        <br/>
-        <h5>Right aligned</h5>
-        <p>To align the nav to the right can be easily achieved by adding the modifier <code>.is-right</code> to the <code>.hx-nav</code> base class.</p>
-
-        <br>
-        <p>Example</p>
-        <ul class="hx-nav is-right">
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link">News & data</a>
-          </li>
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link is-active">Status board</a>
-          </li>
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link">Custom</a>
-          </li>
-        </ul>
-        <br>
-
-        <p>Code</p>
-        <pre><code class="language-markup"><xmp>
-      <ul class="hx-nav is-right">
-        ...
-      </ul>
-        </xmp></code></pre>
-
-      </div>
-    </div>
 
     <hr>
 
-    <div class="hx-columns">
-      <div class="hx-column">
+    <expanding-code-example v-for="(example, i) in examples" key="i"
+      :title="example.title"
+      :code="example.code"
+      :example="example.code"
+      :visible="false"
+      :background="example.bg"
+    ></expanding-code-example>
 
-        <h5>Vertical</h5>
-        <p>Note the vertical nav requires the modifier <code>.is-vertical</code> added to <code>.hx-nav</code> base class.</p>
-
-        <p>Example</p>
-        <ul class="hx-nav is-vertical">
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link">News & data</a>
-          </li>
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link is-active">Status board</a>
-          </li>
-          <li class="hx-nav-item">
-            <a href="#" class="hx-nav-link">Custom</a>
-          </li>
-        </ul>
-
-        <p>Code</p>
-        <pre><code class="language-markup"><xmp>
-      <ul class="hx-nav is-vertical">
-        <li class="hx-nav-item">
-          <a href="#" class="hx-nav-link">News & data</a>
-        </li>
-        <li class="hx-nav-item">
-          <a href="#" class="hx-nav-link is-active">Status board</a>
-        </li>
-        <li class="hx-nav-item">
-          <a href="#" class="hx-nav-link">Custom</a>
-        </li>
-      </ul>
-        </xmp></code></pre>
-
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
+import ExpandingCodeExample from '@/components/ExpandingCodeExample'
 export default {
+  data () {
+    return {
+      examples: [
+        {
+          title: 'Basic nav',
+          code: `
+<p>Note the nav requires the <code>.hx-nav</code> base class.</p>
+<br>
+<ul class="hx-nav">
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link">News & data</a>
+  </li>
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link is-active">Status board</a>
+  </li>
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link">Custom</a>
+  </li>
+</ul>
+          `
+        },
+        {
+          title: 'Alignments',
+          code: `
+<h5>Center aligned</h5>
+<p>Centering the nav can be easily done by adding the modifier <code>.is-centered</code> to the <code>.hx-nav</code> base class.</p>
+<br>
+<ul class="hx-nav is-centered">
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link">News & data</a>
+  </li>
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link is-active">Status board</a>
+  </li>
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link">Custom</a>
+  </li>
+</ul>
+<hr />
+<h5>Right aligned</h5>
+<p>To align the nav to the right can be easily achieved by adding the modifier <code>.is-right</code> to the <code>.hx-nav</code> base class.</p>
+<br>
+<ul class="hx-nav is-right">
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link">News & data</a>
+  </li>
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link is-active">Status board</a>
+  </li>
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link">Custom</a>
+  </li>
+</ul>
+          `
+        },
+        {
+          title: 'Vertical nav',
+          code: `
+<p>Note the vertical nav requires the modifier <code>.is-vertical</code> added to <code>.hx-nav</code> base class.</p>
+<br>
+<ul class="hx-nav is-vertical">
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link">News & data</a>
+  </li>
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link is-active">Status board</a>
+  </li>
+  <li class="hx-nav-item">
+    <a href="#" class="hx-nav-link">Custom</a>
+  </li>
+</ul>
+          `
+        }
+      ]
+    }
+  },
+  components: {
+    ExpandingCodeExample
+  }
 }
 </script>
 
