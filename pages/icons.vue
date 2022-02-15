d8db8a8e7ca86190bdfafa16cb431399cd5428c6
Matthew Curcio <matthew.curcio@medicaldirector.com>
2022-02-15 15:23:16 +1100

    feat(icons): add intray, outtray, task and person-multiple icons

--- pages/icons.vue
+++ pages/icons.vue
@@ -2,64 +2,78 @@
   <div id="icons">
     <h1 class="hx-title">Icons</h1>
     <h2 class="hx-subtitle">Hx/Ui comes with its own custom icons.</h2>
-    <hr>
+    <hr />
 
     <section class="hx-section">
       <div class="hx-columns">
         <div class="hx-column is-6-widescreen is-offset-3-widescreen">
-
           <div class="hx-input-control has-text-centered">
-            <input class="hx-input" type="text" required v-model="searchTerm">
+            <input class="hx-input" type="text" required v-model="searchTerm" />
             <label class="hx-label">
               <i class="hx-icon icon-search is-small"></i>
               Search icons
             </label>
           </div>
-
         </div>
       </div>
     </section>
 
     <section class="hx-section content is-small">
       <div class="hx-columns is-mobile">
-
-        <div class="hx-column is-2 has-text-centered" v-for="icon in filterResults">
+        <div
+          class="hx-column is-2 has-text-centered"
+          v-for="icon in filterResults"
+          :key="icon.name"
+        >
           <i class="hx-icon is-large" :class="'icon-' + icon.name"></i>
           <p v-text="icon.name"></p>
         </div>
-
       </div>
     </section>
 
     <section class="hx-section content is-small">
       <h3>Legacy Icons</h3>
       <div class="hx-columns is-mobile">
-        <div class="hx-column is-2 has-text-centered" v-for="icon in iconsLegacy">
+        <div
+          class="hx-column is-2 has-text-centered"
+          v-for="icon in iconsLegacy"
+          :key="icon.name"
+        >
           <i class="hx-icon is-large" :class="'icon-' + icon.name"></i>
           <p v-text="icon.name"></p>
         </div>
-
       </div>
     </section>
 
-    <hr>
+    <hr />
 
     <section class="hx-section">
-      <p>Because the icons can take a few seconds to load, and because you want control over the space the icons will take, you can use the icon class as a container:</p>
+      <p>
+        Because the icons can take a few seconds to load, and because you want
+        control over the space the icons will take, you can use the icon class
+        as a container:
+      </p>
 
       <p>Usage</p>
 
       <pre v-highlightjs><code class="html">{{ code }}</code></pre>
 
-      <p>The <code>icon</code> container will take up exactly <strong>1.5rem x 1.5rem</strong>. The icon itself is sized at <strong>21px</strong>.</p>
+      <p>
+        The <code>icon</code> container will take up exactly
+        <strong>1.5rem x 1.5rem</strong>. The icon itself is sized at
+        <strong>21px</strong>.
+      </p>
     </section>
 
-    <hr>
+    <hr />
 
     <section class="hx-section">
-
       <h3>Sizes</h3>
-      <p>Font Awesome icons use a font-size of 28px by default, and are best rendered when using multiples of 7. The icon container is always slightly bigger than the font-size used:</p>
+      <p>
+        Font Awesome icons use a font-size of 28px by default, and are best
+        rendered when using multiples of 7. The icon container is always
+        slightly bigger than the font-size used:
+      </p>
       <table class="hx-table is-striped">
         <thead>
           <tr>
@@ -72,25 +86,33 @@
         <tbody>
           <tr>
             <td><code>hx-icon is-small</code></td>
-            <td class="has-text-centered"><i class="hx-icon icon-helix is-small"></i></td>
+            <td class="has-text-centered">
+              <i class="hx-icon icon-helix is-small"></i>
+            </td>
             <td>14px</td>
             <td>1rem x 1rem</td>
           </tr>
           <tr>
             <td><code>hx-icon</code></td>
-            <td class="has-text-centered"><i class="hx-icon icon-helix"></i></td>
+            <td class="has-text-centered">
+              <i class="hx-icon icon-helix"></i>
+            </td>
             <td>21px</td>
             <td>1.5rem x 1.5rem</td>
           </tr>
           <tr>
             <td><code>hx-icon is-medium</code></td>
-            <td class="has-text-centered"><i class="hx-icon icon-helix is-medium"></i></td>
+            <td class="has-text-centered">
+              <i class="hx-icon icon-helix is-medium"></i>
+            </td>
             <td>28px</td>
             <td>2rem x 2rem</td>
           </tr>
           <tr>
             <td><code>hx-icon is-large</code></td>
-            <td class="has-text-centered"><i class="hx-icon icon-helix is-large"></i></td>
+            <td class="has-text-centered">
+              <i class="hx-icon icon-helix is-large"></i>
+            </td>
             <td>42px</td>
             <td>3rem x 3rem</td>
           </tr>
@@ -108,234 +130,251 @@ export default {
       filterResults: this.icons,
       code: '<i class="hx-icon icon-helix"></i>',
       icons: [
-        { name: 'helix' },
-        { name: 'recall' },
-        { name: 'power' },
-        { name: 'login' },
-        { name: 'logout' },
-        { name: 'navicon' },
-        { name: 'language' },
-        { name: 'batch' },
+        { name: 'lock-outline' },
+        { name: 'lock' },
+        { name: 'send-outline' },
         { name: 'batch-outline' },
-        { name: 'bell' },
+        { name: 'batch' },
         { name: 'bell-outline' },
-        { name: 'body' },
+        { name: 'bell' },
         { name: 'body-outline' },
-        { name: 'box' },
+        { name: 'body' },
         { name: 'box-outline' },
-        { name: 'chat-bubble' },
+        { name: 'box' },
+        { name: 'appointment-outline' },
+        { name: 'calendar-outline' },
+        { name: 'appointment' },
+        { name: 'calendar' },
         { name: 'chat-bubble-outline' },
-        { name: 'circle' },
-        { name: 'circle-outline' },
-        { name: 'square' },
-        { name: 'square-outline' },
-        { name: 'check' },
-        { name: 'check-outline' },
+        { name: 'chat-bubble' },
         { name: 'check-empty' },
-        { name: 'close' },
-        { name: 'close-outline' },
-        { name: 'close-empty' },
-        { name: 'minus' },
-        { name: 'minus-outline' },
-        { name: 'minus-empty' },
-        { name: 'plus' },
-        { name: 'plus-outline' },
-        { name: 'plus-empty' },
-        { name: 'information' },
-        { name: 'information-outline' },
-        { name: 'information-empty' },
-        { name: 'help' },
-        { name: 'help-outline' },
-        { name: 'help-empty' },
-        { name: 'clock' },
+        { name: 'check-outline' },
+        { name: 'check' },
         { name: 'clock-outline' },
-        { name: 'clock-minus' },
-        { name: 'clock-minus-outline' },
-        { name: 'cloud-download' },
+        { name: 'clock' },
+        { name: 'close-empty' },
+        { name: 'close-outline' },
+        { name: 'close' },
         { name: 'cloud-download-outline' },
-        { name: 'cloud-upload' },
+        { name: 'cloud-download' },
         { name: 'cloud-upload-outline' },
-        { name: 'email' },
+        { name: 'cloud-upload' },
         { name: 'email-outline' },
-        { name: 'exclamation' },
+        { name: 'email' },
         { name: 'exclamation-outline' },
-        { name: 'file' },
+        { name: 'alert-outline' },
+        { name: 'exclamation' },
+        { name: 'alert' },
         { name: 'file-outline' },
-        { name: 'filing' },
+        { name: 'file' },
         { name: 'filing-outline' },
-        { name: 'flag' },
+        { name: 'filing' },
         { name: 'flag-outline' },
-        { name: 'heart' },
-        { name: 'heart-outline' },
+        { name: 'flag' },
         { name: 'flask' },
         { name: 'flask-reverse' },
-        { name: 'list' },
-        { name: 'list-outline' },
-        { name: 'carrot' },
-        { name: 'carrot-outline' },
-        { name: 'nutrition' },
-        { name: 'nutrition-outline' },
-        { name: 'pie' },
-        { name: 'pie-outline' },
-        { name: 'telephone' },
-        { name: 'telephone-outline' },
-        { name: 'alcohol' },
-        { name: 'alcohol-outline' },
-        { name: 'wine' },
-        { name: 'wine-outline' },
-        { name: 'patient' },
-        { name: 'patient-outline' },
-        { name: 'patient-add' },
-        { name: 'patient-add-outline' },
-        { name: 'person' },
-        { name: 'person-outline' },
-        { name: 'doctor' },
-        { name: 'doctor-outline' },
-        { name: 'doctor-check' },
-        { name: 'doctor-check-outline' },
-        { name: 'printer' },
-        { name: 'printer-outline' },
-        { name: 'stopwatch' },
-        { name: 'stopwatch-outline' },
-        { name: 'star' },
-        { name: 'star-outline' },
-        { name: 'queue' },
-        { name: 'search' },
+        { name: 'heart-outline' },
+        { name: 'heart' },
+        { name: 'helix' },
+        { name: 'help-empty' },
+        { name: 'help-outline' },
+        { name: 'help' },
         { name: 'injection' },
-        { name: 'heartbeat' },
-        { name: 'pulse' },
-        { name: 'medications' },
         { name: 'iphone' },
         { name: 'mobile' },
-        { name: 'credit-card' },
+        { name: 'list-outline' },
+        { name: 'list' },
+        { name: 'medications' },
+        { name: 'minus-empty' },
+        { name: 'minus-outline' },
+        { name: 'minus' },
+        { name: 'nutrition-outline' },
+        { name: 'carrot-outline' },
+        { name: 'nutrition' },
+        { name: 'carrot' },
+        { name: 'patient-add-outline' },
+        { name: 'patient-add' },
+        { name: 'patient-outline' },
+        { name: 'patient' },
+        { name: 'doctor-check-outline' },
+        { name: 'doctor-check' },
+        { name: 'pie-outline' },
+        { name: 'pie' },
+        { name: 'compass' },
+        { name: 'plus-outline' },
+        { name: 'plus' },
+        { name: 'printer-outline' },
+        { name: 'printer' },
+        { name: 'heartbeat' },
+        { name: 'pulse' },
+        { name: 'queue' },
+        { name: 'search' },
+        { name: 'star-outline' },
+        { name: 'star' },
+        { name: 'stopwatch-outline' },
+        { name: 'stopwatch' },
+        { name: 'telephone-outline' },
+        { name: 'telephone' },
+        { name: 'alcohol-outline' },
+        { name: 'wine-outline' },
+        { name: 'alcohol' },
+        { name: 'wine' },
+        { name: 'information-empty' },
+        { name: 'information-outline' },
+        { name: 'information' },
         { name: 'cash' },
-        { name: 'attachment' },
-        { name: 'paperclip' },
-        { name: 'settings' },
-        { name: 'settings-outline' },
-        { name: 'cog' },
-        { name: 'cog-outline' },
-        { name: 'note' },
         { name: 'note-outline' },
-        { name: 'man' },
-        { name: 'woman' },
-        { name: 'male' },
-        { name: 'female' },
-        { name: 'more' },
         { name: 'more-vertical' },
-        { name: 'grid' },
-        { name: 'sort' },
+        { name: 'caret-down' },
+        { name: 'caret-up' },
+        { name: 'vue' },
+        { name: 'html-css' },
+        { name: 'more' },
+        { name: 'angular' },
+        { name: 'credit-card' },
+        { name: 'paperclip' },
+        { name: 'attachment' },
+        { name: 'note' },
+        { name: 'settings-outline' },
+        { name: 'cog-outline' },
+        { name: 'recall' },
+        { name: 'login' },
+        { name: 'logout' },
+        { name: 'power' },
+        { name: 'angle-left' },
+        { name: 'angle-down' },
+        { name: 'angle-right' },
+        { name: 'angle-up' },
         { name: 'sort-asc' },
         { name: 'sort-desc' },
-        { name: 'caret-up' },
-        { name: 'caret-down' },
-        { name: 'arrow-up' },
-        { name: 'arrow-right' },
+        { name: 'sort' },
+        { name: 'caret-left' },
+        { name: 'caret-right' },
+        { name: 'filter-outline' },
+        { name: 'funnel-outline' },
+        { name: 'filter' },
+        { name: 'funnel' },
+        { name: 'today-outline' },
+        { name: 'today' },
+        { name: 'grid' },
+        { name: 'settings' },
+        { name: 'cog' },
+        { name: 'react' },
+        { name: 'dashboard-outline' },
+        { name: 'speedometer-outline' },
+        { name: 'dashboard' },
+        { name: 'speedometer' },
+        { name: 'male' },
+        { name: 'man' },
+        { name: 'woman' },
+        { name: 'female' },
+        { name: 'language' },
+        { name: 'navicon' },
+        { name: 'circle' },
         { name: 'arrow-down' },
         { name: 'arrow-left' },
-        { name: 'angle-up' },
-        { name: 'angle-right' },
-        { name: 'angle-down' },
-        { name: 'angle-left' },
-        { name: 'dashboard' },
-        { name: 'dashboard-outline' },
-        { name: 'speedometer' },
-        { name: 'speedometer-outline' },
-        { name: 'appointment' },
-        { name: 'appointment-outline' },
-        { name: 'calendar' },
-        { name: 'calendar-outline' },
-        { name: 'today' },
-        { name: 'today-outline' },
-        { name: 'filter' },
-        { name: 'filter-outline' },
-        { name: 'funnel' },
-        { name: 'funnel-outline' },
+        { name: 'arrow-right' },
+        { name: 'arrow-up' },
+        { name: 'circle-outline' },
+        { name: 'square-outline' },
+        { name: 'square' },
         { name: 'code' },
-        { name: 'lock' },
-        { name: 'lock-outline' },
         { name: 'bin' },
         { name: 'bin-outline' },
-        { name: 'send' },
-        { name: 'send-outline' },
-        { name: 're-send' },
-        { name: 'home' },
-        { name: 'home-outline' },
-        { name: 'compass-outline' },
-        { name: 'compass' },
+        { name: 'imaging-outline' },
+        { name: 'radiology-outline' },
+        { name: 'prescription' },
+        { name: 'external-link' },
+        { name: 'doctor-outline' },
+        { name: 'doctor' },
+        { name: 'clock-minus' },
+        { name: 'person' },
+        { name: 'person-outline' },
+        { name: 'clock-minus-outline' },
+        { name: 'did-not-wait' },
         { name: 'edit-outline' },
         { name: 'edit' },
         { name: 'recalls' },
+        { name: 'home-outline' },
+        { name: 'home' },
         { name: 'pathology-outline' },
         { name: 'pathology' },
-        { name: 'medications' },
-        { name: 'imaging-outline' },
-        { name: 'imaging' },
-        { name: 'radiology-outline' },
-        { name: 'radiology' },
-        { name: 'prescription' },
-        { name: 'external-link' },
-        { name: 'did-not-wait' },
         { name: 'linked' },
+        { name: 'radio-button' },
+        { name: 'checkbox' },
         { name: 'dropdown' },
         { name: 'textarea' },
         { name: 'textbox' },
-        { name: 'radio-button' },
-        { name: 'checkbox' },
+        { name: 'clone-outline' },
+        { name: 're-print' },
+        { name: 'imaging' },
+        { name: 'radiology' },
+        { name: 'compass-outline' },
+        { name: 'clipboard-bullets-outline' },
+        { name: 'clipboard-bullets' },
+        { name: 'clipboard-heart-outline' },
+        { name: 'clipboard-heart' },
         { name: 'clipboard' },
         { name: 'clipboard-outline' },
-        { name: 'clipboard-heart' },
-        { name: 'clipboard-heart-outline' },
-        { name: 'clipboard-bullets' },
-        { name: 'clipboard-bullets-outline' },
-        { name: 'fullscreen' },
         { name: 'fullscreen-close' },
-        { name: 'letter' },
-        { name: 'letter-outline' },
+        { name: 'fullscreen' },
+        { name: 'claim-dollar-circle' },
+        { name: 'plus-empty' },
         { name: 'letter-tick' },
         { name: 'letter-tick-outline' },
         { name: 'letter-certificate' },
         { name: 'letter-certificate-outline' },
         { name: 'letter-signature' },
         { name: 'letter-signature-outline' },
-        { name: 'buildings' },
         { name: 'buildings-outline' },
-        { name: 'file-certificate' },
+        { name: 'buildings' },
+        { name: 'letter' },
+        { name: 'letter-outline' },
         { name: 'file-certificate-outline' },
-        { name: 'signature' },
+        { name: 'file-certificate' },
         { name: 'signature-outline' },
-        { name: 'allergy' },
+        { name: 'signature' },
+        { name: 'address-book' },
         { name: 'allergy-outline' },
-        { name: 'medications-alerts' },
+        { name: 'allergy' },
         { name: 'medications-alerts-outline' },
+        { name: 'medications-alerts' },
         { name: 'table' },
         { name: 'table-outline' },
-        { name: 'card-medicare' },
+        { name: 'card-blank' },
+        { name: 'card-medicare-m' },
         { name: 'card-dva' },
-        { name: 'card-dva-gold' },
-        { name: 'card-dva-orange' },
-        { name: 'card-dva-white' },
+        { name: 'card-dva-outlined' },
         { name: 'card-cc' },
-        { name: 'card-hcc' },
         { name: 'card-scc' },
+        { name: 'card-pcc' },
+        { name: 'card-hcc' },
+        { name: 'send' },
         { name: 'virus' },
-        { name: 'video' },
         { name: 'video-outline' },
+        { name: 'video' },
+        { name: 're-send' },
+        { name: 'invisible' },
         { name: 'visible' },
-        { name: 'invisible' }
+        { name: 'intray' },
+        { name: 'intray-outline' },
+        { name: 'outtray' },
+        { name: 'outtray-outline' },
+        { name: 'task-outline' },
+        { name: 'person-multiple' }
       ],
       iconsLegacy: [
         { name: 'legacy-discharge' },
         { name: 'legacy-pathology' },
         { name: 'legacy-radiology' },
-        { name: 'legacy-letter-referral' },
-        { name: 'legacy-letter-other' },
         { name: 'legacy-workers-compensation' },
-        { name: 'legacy-letter-writer' },
         { name: 'legacy-letter-specialist' },
-        { name: 'legacy-mhr' },
+        { name: 'legacy-letter-referral' },
+        { name: 'legacy-cdm' },
         { name: 'legacy-smd' },
-        { name: 'legacy-cdm' }
+        { name: 'legacy-mhr' },
+        { name: 'legacy-letter-other' },
+        { name: 'legacy-letter-writer' }
       ]
     }
   },
@@ -347,8 +386,10 @@ export default {
       if (this.searchTerm.length > 0) {
         this.filterResults = []
         for (let icon of this.icons) {
-          if (icon.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) >= 0) {
-            this.filterResults.push({ 'name': icon.name })
+          if (
+            icon.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) >= 0
+          ) {
+            this.filterResults.push({ name: icon.name })
           }
         }
       } else {
