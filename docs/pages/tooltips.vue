c347ab3801fd1d28519d73d1ab8f6a47e65152aa
Matthew Curcio <matthew.curcio@medicaldirector.com>
2022-10-12 11:55:59 +1100

    wip - tooltip

--- docs/pages/tooltips.vue
+++ docs/pages/tooltips.vue
@@ -1,5 +1,5 @@
 <template>
-  <article class="hx-section scrollable-y">
+  <article class="hx-section scrollable-y" style="min-height: 100%">
     <div class="hx-prose contents">
       <h1 class="hx-title pt-4">Tooltips</h1>
       <h2 class="hx-subtitle">
@@ -9,6 +9,8 @@
     </div>
 
     <hr />
+
+    <api-table :data="data"></api-table>
 
     <div class="grid gap-4">
       <expanding-code-example
@@ -27,177 +29,129 @@
 
 <script>
 import ExpandingCodeExample from '../components/ExpandingCodeExample.vue'
+import ApiTable from '../components/ApiTable.vue'
 
 export default {
   components: {
     ExpandingCodeExample,
+    ApiTable,
   },
   data() {
     return {
+      data: [
+        {
+          class: 'hx-tooltip-container',
+          type: 'component',
+          description: 'Container element (optional)',
+        },
+        {
+          class: 'hoverable',
+          type: 'modifier',
+          description: 'Makes tooltip visible on hover',
+        },
+        {
+          class: 'hx-tooltip[role="tooltip"]',
+          type: 'component',
+          description:
+            'Main element. Requires adding element attribute of role="tooltip".',
+        },
+        {
+          class: 'contrast',
+          type: 'modifier',
+          description: 'Neutral background in light mode',
+        },
+        {
+          class: 'active',
+          type: 'modifier',
+          description: 'Makes tooltip visible',
+        },
+        {
+          class: 'hx-tooltip-content',
+          type: 'component',
+          description: 'Content element (class is optional, element is not)',
+        },
+      ],
       examples: [
         {
-          title: 'Example',
-          code: `
-  <p>The tooltip structure is simple:</p>
-  <ul>
-    <li>
-      <code>.hx-tooltip</code>: The tooltip container
-      <ul>
-        <li>
-          <code>.hx-tooltip-content</code>: The tooltip content
-        </li>
-      </ul>
-    </li>
-  </ul>
-  <br />
-  <div class="hx-example has-text-centered">
-    <div class="hx-tooltip is-active">
-      <div class="hx-tooltip-content">
+          title: 'Basic',
+          withContent: false,
+          code: `<div class="hx-card-content grid grid-cols-2">
+
+  <div class="hx-flex justify-center">
+    <div class="hx-tooltip active" role="tooltip">
+      <span>
         A Simple Tooltip
-      </div>
+      </span>
     </div>
   </div>
-            `,
+
+  <div class="hx-flex justify-center">
+    <div class="hx-tooltip active contrast" role="tooltip">
+      <span>
+        A Simple Tooltip
+      </span>
+    </div>
+  </div>
+  
+</div>`,
         },
         {
-          title: 'Arrow positions',
-          code: `
-  <p>There are four modifiers to change the tooltip arrow alignment: <br /><code>.is-top</code>, <code>.is-right</code>, <code>.is-bottom</code>, and <code>.is-left</code></p>
-  <br />
-  <div class="hx-example has-text-centered">
-    <div class="hx-tooltip is-top is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the top
-      </div>
-    </div>
-    <div class="hx-tooltip is-right is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the right
-      </div>
-    </div>
-    <div class="hx-tooltip is-bottom is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the bottom
-      </div>
-    </div>
-    <div class="hx-tooltip is-left is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the left
-      </div>
+          title: 'Hover',
+          withContent: false,
+          code: `<div class="hx-card-content flex grow-1 justify-center">
+  <div class="hx-tooltip-container hoverable">
+    <span>Hover over me!</span>
+    <div class="hx-tooltip" role="tooltip">
+      <span>
+        Contrasting background
+      </span>
     </div>
   </div>
-            `,
+</div>`,
         },
         {
-          title: 'Contextual',
-          code: `
-  <p>Add contextual class modifiers <code>is-white</code>, <code>is-danger</code>, <code>is-warning</code> or  <code>is-success</code> to change the colour of the tooltip.</p>
-  <br />
-  <div class="hx-example has-text-centered">
-    <div class="hx-tooltip is-top is-white is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the top
-      </div>
-    </div>
-    <div class="hx-tooltip is-right is-white is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the right
-      </div>
-    </div>
-    <div class="hx-tooltip is-bottom is-white is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the bottom
-      </div>
-    </div>
-    <div class="hx-tooltip is-left is-white is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the left
+          title: 'Position (on hover)',
+          withContent: false,
+          code: `<div class="hx-card-content grid phablet:grid-cols-2 tablet:grid-cols-4 gap-4">
+  <div class="hx-flex justify-center">
+    <div class="hx-tooltip-container hoverable">
+      <span>Hover over me!</span>
+      <div class="hx-tooltip" role="tooltip">
+        <span>Bottom</span>
       </div>
     </div>
   </div>
-  <br />
-  <div class="hx-example has-text-centered">
-    <div class="hx-tooltip is-top is-success is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the top
-      </div>
-    </div>
-    <div class="hx-tooltip is-right is-success is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the right
-      </div>
-    </div>
-    <div class="hx-tooltip is-bottom is-success is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the bottom
-      </div>
-    </div>
-    <div class="hx-tooltip is-left is-success is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the left
+
+  <div class="hx-flex justify-center">
+    <div class="hx-tooltip-container hoverable">
+      <span>Hover over me!</span>
+      <div class="hx-tooltip top" role="tooltip">
+        <span>Top</span>
       </div>
     </div>
   </div>
-  <br />
-  <div class="hx-example has-text-centered">
-    <div class="hx-tooltip is-top is-warning is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the top
-      </div>
-    </div>
-    <div class="hx-tooltip is-right is-warning is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the right
-      </div>
-    </div>
-    <div class="hx-tooltip is-bottom is-warning is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the bottom
-      </div>
-    </div>
-    <div class="hx-tooltip is-left is-warning is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the left
+
+  <div class="hx-flex justify-center">
+    <div class="hx-tooltip-container hoverable">
+      <span>Hover over me!</span>
+      <div class="hx-tooltip right" role="tooltip">
+        <span>Right</span>
       </div>
     </div>
   </div>
-  <br />
-  <div class="hx-example has-text-centered">
-    <div class="hx-tooltip is-top is-danger is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the top
-      </div>
-    </div>
-    <div class="hx-tooltip is-right is-danger is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the right
-      </div>
-    </div>
-    <div class="hx-tooltip is-bottom is-danger is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the bottom
-      </div>
-    </div>
-    <div class="hx-tooltip is-left is-danger is-active">
-      <div class="hx-tooltip-content">
-        Tooltip on the left
+
+  <div class="hx-flex justify-center">
+    <div class="hx-tooltip-container hoverable">
+      <span>Hover over me!</span>
+      <div class="hx-tooltip left" role="tooltip">
+        <span>Left</span>
       </div>
     </div>
   </div>
-            `,
+</div>`,
         },
       ],
     }
   },
 }
 </script>
-
-<style lang="scss">
-.hx-example {
-  .hx-tooltip {
-    display: inline-block;
-    margin: 0 1rem;
-    position: relative;
-  }
-}
-</style>
